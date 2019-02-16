2305a6cd18d143eadb99975f42e4140839d39893
Keith Makan <k3170makan@gmail.com>
2019-02-16 01:35:48 -0800

    Finished DLL charactersistics field

--- libheader/PEImageOptHeader.py
+++ libheader/PEImageOptHeader.py
@@ -9,20 +9,34 @@ class PEImageOptHeader:
 	__PEImageOptHeader_magic_versions = {0x10B:"32 bit binary",\
 														0x20B:"64 bit binary"}
 	__PEImageOptHeader_subsys_types = {\
-									0 :"IMAGE_SUBSYSTEM_UNKNOWN ",
-									1 :"IMAGE_SUBSYSTEM_NATIVE ",
-									2 :"IMAGE_SUBSYSTEM_WINDOWS_GUI ",
-									3 :"IMAGE_SUBSYSTEM_WINDOWS_CUI ",
-									5 :"IMAGE_SUBSYSTEM_OS2_CUI ",
-									7 :"IMAGE_SUBSYSTEM_POSIX_CUI ",
-									8 :"IMAGE_SUBSYSTEM_NATIVE_WINDOWS ",
-									9 :"IMAGE_SUBSYSTEM_WINDOWS_CE_GUI ",
-									10 :"IMAGE_SUBSYSTEM_EFI_APPLICATION ",
-									11 :"IMAGE_SUBSYSTEM_EFI_BOOT_ SERVICE_DRIVER ",
-									12 :"IMAGE_SUBSYSTEM_EFI_RUNTIME_ DRIVER ",
-									13 :"IMAGE_SUBSYSTEM_EFI_ROM ",
-									14 :"IMAGE_SUBSYSTEM_XBOX ",
-									16 :"IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION"}
+					0 :"IMAGE_SUBSYSTEM_UNKNOWN ",\
+					1 :"IMAGE_SUBSYSTEM_NATIVE ",\
+					2 :"IMAGE_SUBSYSTEM_WINDOWS_GUI ",\
+					3 :"IMAGE_SUBSYSTEM_WINDOWS_CUI ",\
+					5 :"IMAGE_SUBSYSTEM_OS2_CUI ",\
+					7 :"IMAGE_SUBSYSTEM_POSIX_CUI ",\
+					8 :"IMAGE_SUBSYSTEM_NATIVE_WINDOWS ",\
+					9 :"IMAGE_SUBSYSTEM_WINDOWS_CE_GUI ",\
+					10 :"IMAGE_SUBSYSTEM_EFI_APPLICATION ",\
+					11 :"IMAGE_SUBSYSTEM_EFI_BOOT_ SERVICE_DRIVER ",\
+					12 :"IMAGE_SUBSYSTEM_EFI_RUNTIME_ DRIVER ",\
+					13 :"IMAGE_SUBSYSTEM_EFI_ROM ",\
+					14 :"IMAGE_SUBSYSTEM_XBOX ",\
+					16 :"IMAGE_SUBSYSTEM_WINDOWS_BOOT_APPLICATION"}
+
+	__PEImageOptHeader_dllchar_types = {\
+									"IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA":0x0020,\
+									"IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE":0x0040,\
+									"IMAGE_DLLCHARACTERISTICS_FORCE_INTEGRITY":0x0080,\
+									"IMAGE_DLLCHARACTERISTICS_NX_COMPAT":0x0100,\
+									"IMAGE_DLLCHARACTERISTICS_NO_ISOLATION":0x0200,\
+									"IMAGE_DLLCHARACTERISTICS_NO_SEH":0x0400,\
+									"IMAGE_DLLCHARACTERISTICS_NO_BIND":0x0800,\
+									"IMAGE_DLLCHARACTERISTICS_APPCONTAINER":0x1000,\
+									"IMAGE_DLLCHARACTERISTICS_WDM_DRIVER":0x2000,\
+									"IMAGE_DLLCHARACTERISTICS_GUARD_CF":0x4000,\
+									"IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE":0x8000}
+
 	__PEImageOptHeader_fmt_dict = {"Magic":"H",\
 							"LinkerVersion":"H",\
 							"SizeOfCode":"I",
@@ -96,6 +110,7 @@ class PEImageOptHeader:
 		self.header_fmt_dict = PEImageOptHeader.__PEImageOptHeader_fmt_dict
 		self.header_versions = PEImageOptHeader.__PEImageOptHeader_magic_versions
 		self.header_subsversions = PEImageOptHeader.__PEImageOptHeader_subsys_types
+		self.header_dllchars = PEImageOptHeader.__PEImageOptHeader_dllchar_types
 
 	def build_from_binary(self,_filename,_fileperms="rb"):
 		self.filename = _filename
@@ -119,14 +134,22 @@ class PEImageOptHeader:
 		self.filename = self.dos_header.filename
 		self.fileperms = "rb"
 		self.e_lfanew = int(self.dos_header.get_e_lfanew(),16)
-		
 		optheader = PEImageOptHeaderDecoder.Decoder(_filename=self.filename,\
                                          _fileperms=self.fileperms)
 		
 		for index,value in enumerate(optheader.decode(_start=(self.e_lfanew+16+8))):
-			self.attribute_list[index] = \
-					(self.attribute_list[index][0],\
-					value)
+
+			if (self.attribute_list[index][0] == "DLLCharacteristics"):
+				for char in self.header_dllchars:
+					char_value = self.header_dllchars[char]
+					and_value = char_value & value
+					if (and_value):
+						if len(self.attribute_list[index]) == 3:
+							self.attribute_list[index][2].append(char)
+						else:
+							self.attribute_list[index] = (self.attribute_list[index][0],value,[char])
+			else:
+				self.attribute_list[index] = (self.attribute_list[index][0],value)
 
 		return self.attribute_list
 	
@@ -138,6 +161,15 @@ class PEImageOptHeader:
 					doc_string += " \t\t|- %s => [%s : %s]\n" % (field,\
 							hex(self.attribute_list[index][1]),\
 							self.header_subsversions[self.attribute_list[index][1]])
+			
+				elif (field == "DLLCharacteristics"):
+					doc_string += " \t\t|- %s => [%s : %s]\n" % (field,\
+							hex(self.attribute_list[index][1]),\
+							self.attribute_list[index][1])
+
+					if(len(self.attribute_list[index]) == 3):
+						for charac in self.attribute_list[index][2]:
+							doc_string += "\t\t\t|-- [%s]\n" % (charac)
 				elif (field == "Magic"):
 						doc_string += " \t\t|- %s => [%s : %s]\n" % (field,\
 							hex(self.attribute_list[index][1]),\
@@ -146,9 +178,8 @@ class PEImageOptHeader:
 					doc_string += " \t\t|- %s => [%s : %s]\n" % (field,\
 							hex(self.attribute_list[index][1]),\
 							self.attribute_list[index][1].__repr__())
-			except KeyError:
+			except:
 				doc_string += " \t\t|- %s => [%s : %s]\n" % (field,\
 						hex(self.attribute_list[index][1]),\
-						self.attribute_list[index][1].__repr__())
-
+						self.attribute_list[index][1])
 		return doc_string
